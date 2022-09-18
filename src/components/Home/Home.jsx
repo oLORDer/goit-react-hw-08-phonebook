aa69e2d71c429a780d46fcae688896ae17eebf96
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-18 17:20:23 +0300

    styled

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,18 @@
+import s from './home.module.scss';
+
 const Home = () => {
   return (
     <>
-      <h1>HomePage</h1>
-      <p>Welcome</p>
+      <div>
+        <h1 className={s.h1}>
+          <em>C</em>
+          <em className={`${s.planet} ${s.left}`}>O</em>
+          <em>S</em>
+          <em>M</em>
+          <em className={`${s.planet} ${s.right}`}>O</em>
+          <em>S</em>
+        </h1>
+      </div>
     </>
   );
 };
